fe4482a08d209267f27aedaf45acf4ff628e419c
DHaylock <david.haylock@watershed.co.uk>
2015-12-13 23:52:01 +0000

    - Added: Pre existing class Timer with methods

--- H_A_R_P_S/src/local_addons/MyTimer.hpp
+++ H_A_R_P_S/src/local_addons/MyTimer.hpp
@@ -13,4 +13,26 @@
 #include <stdio.h>
 #include "ofMain.h"
 
+class MyTimer {
+    public:
+        void setup(float timerLength,string timerName,bool loop,string fontFile);
+        void update();
+        void draw(int x, int y);
+        
+        void start();
+        void stop();
+        void reset();
+        void setNewTimerLength(int timerLength);
+    
+    private:
+        float startTime;
+        float endTime;
+        float timeLeft;
+        bool  bTimerReached;
+        bool _loop;
+        float _timerLength;
+    
+        string _timerName;
+        ofTrueTypeFont font;
+};
 #endif /* MyTimer_hpp */
