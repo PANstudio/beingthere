6cc03015bfebcd11852227161ba1d8feb6c246c5
DHaylock <david.haylock@watershed.co.uk>
2015-12-16 01:12:20 +0000

    - Added: base of player manager system

--- H_A_R_P_S/src/local_addons/PlayerManager.cpp
+++ H_A_R_P_S/src/local_addons/PlayerManager.cpp
@@ -6,3 +6,36 @@
 //  Created by David Haylock on 11/11/2015.
 //
 //--------------------------------------------------------------
+#include "PlayerManager.hpp"
+//--------------------------------------------------------------
+void PlayerManager::setup(string address, int port)
+{
+    oscReceiver.setup(port);
+}
+//--------------------------------------------------------------
+void PlayerManager::listen()
+{
+    while (oscReceiver.hasWaitingMessages()) {
+        ofxOscMessage m;
+        oscReceiver.getNextMessage(m);
+        
+        for(int i = 0; i < 3; i++)
+        {
+            if (m.getAddress() == "/player"+ofToString(i)) {
+                
+            }
+        }
+    }
+}
+//--------------------------------------------------------------
+ofPoint PlayerManager::getPlayerCoords()
+{
+    ofPoint pos;
+    return pos;
+}
+//--------------------------------------------------------------
+vector<ofPoint> PlayerManager::getPlayersCoords()
+{
+    vector<ofPoint> pos;
+    return pos;
+}
--- H_A_R_P_S/src/local_addons/PlayerManager.hpp
+++ H_A_R_P_S/src/local_addons/PlayerManager.hpp
@@ -12,5 +12,23 @@
 
 #include <stdio.h>
 #include "ofMain.h"
+#include "ofxOsc.h"
 
+class PlayerManager {
+public:
+    void setup(string address,int port);
+    void listen();
+    void closeListener();
+    
+    string getDebugData();
+    
+    ofPoint getPlayerCoords();
+    vector<ofPoint> getPlayersCoords();
+    
+private:
+    ofxOscReceiver oscReceiver;
+    
+protected:
+    
+};
 #endif /* PlayerManager_hpp */
