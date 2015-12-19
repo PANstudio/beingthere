cefdd77b212de9788caa5bafe56614200032e391
DHaylock <david.haylock@watershed.co.uk>
2015-12-19 23:51:01 +0000

    - Added: Styled Map Class

---
+++ H_A_R_P_S/src/local_addons/StyledMap.cpp
@@ -0,0 +1,9 @@
+//
+//  StyledMap.cpp
+//  H_A_R_P_S
+//
+//  Created by David Haylock on 19/12/2015.
+//
+//
+
+#include "StyledMap.hpp"
---
+++ H_A_R_P_S/src/local_addons/StyledMap.hpp
@@ -0,0 +1,14 @@
+//
+//  StyledMap.hpp
+//  H_A_R_P_S
+//
+//  Created by David Haylock on 19/12/2015.
+//
+//
+
+#ifndef StyledMap_hpp
+#define StyledMap_hpp
+
+#include <stdio.h>
+
+#endif /* StyledMap_hpp */
