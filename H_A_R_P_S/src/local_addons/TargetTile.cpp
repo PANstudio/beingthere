74ec1a75a1241849f45e1ce3ff32dac163fb79c0
DHaylock <david.haylock@watershed.co.uk>
2015-12-30 01:08:35 +0000

    - Starting target tile

--- H_A_R_P_S/src/local_addons/TargetTile.cpp
+++ H_A_R_P_S/src/local_addons/TargetTile.cpp
@@ -6,3 +6,36 @@
 //  Created by David Haylock on 11/11/2015.
 //
 //--------------------------------------------------------------
+#include "TargetTile.hpp"
+//--------------------------------------------------------------
+void TargetTile::setTargetPosition(ofPoint center,int size,int mapWidth,int mapHeight)
+{
+    _center = center;
+    _size = size;
+    _mapWidth = mapWidth;
+    _mapHeight = mapHeight;
+}
+//--------------------------------------------------------------
+vector<Tile> TargetTile::getTargetTiles(Tile tile)
+{
+    vector<Tile> neighbours;
+    for (int x = -1; x <= 1; x++) {
+        for (int y = -1; y <= 1; y++) {
+            if ((x == 0 && y == 0)) {
+                continue;
+            }
+            
+            int checkX = tile.gridX + x;
+            int checkY = tile.gridY + y;
+            if (checkX >= 0 && checkX < _mapWidth && checkY >= 0 && checkY < _mapHeight) {
+//                neighbours.push_back(map[checkX][checkY]);
+            }
+            else {
+                
+            }
+        }
+    }
+    return neighbours;
+}
+//--------------------------------------------------------------
+
--- H_A_R_P_S/src/local_addons/TargetTile.hpp
+++ H_A_R_P_S/src/local_addons/TargetTile.hpp
@@ -12,5 +12,21 @@
 
 #include <stdio.h>
 #include "ofMain.h"
+#include "Tile.hpp"
+
+class TargetTile {
+    public:
+        void setTargetPosition(ofPoint center,int size,int mapWidth,int mapHeight);
+        vector<Tile> getTargetTiles(Tile tile);
+
+    private:
+        vector<Tile> tiles;
+        int _size;
+        ofPoint _center;
+        int _mapWidth;
+        int _mapHeight;
+    
+    protected:
+};
 
 #endif /* TargetTile_h */
