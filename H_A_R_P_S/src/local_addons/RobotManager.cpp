aa47b195c4f4392526a7d7a73880742e638f3844
DHaylock <david.haylock@watershed.co.uk>
2016-04-19 23:36:44 +0100

    - Added: Functions to robot manager - Maybe thread class

--- H_A_R_P_S/src/local_addons/RobotManager.cpp
+++ H_A_R_P_S/src/local_addons/RobotManager.cpp
@@ -6,3 +6,71 @@
 //  Created by David Haylock on 14/11/2015.
 //
 //--------------------------------------------------------------
+#include "RobotManager.h"
+
+//--------------------------------------------------------------
+void RobotManager::setup()
+{
+    ofxOscSender sender;
+    for (int i = 0; i < receiversHostname.size(); i++) {
+        sender.setup(receiversHostname[i], 7890);
+        senders.insert(std::pair<string, ofxOscSender>(receiversHostname[i],sender));
+    }
+    commandsSent.resize(receiversHostname.size());
+}
+//--------------------------------------------------------------
+void RobotManager::loadCommands()
+{
+    ofxJSONElement commandFile;
+    
+    if(!commandFile.open("commands.json")) {
+        ofLogError() << "File not Loaded: COMMANDS";
+        return;
+    }
+    else {
+        int noRobots = commandFile["Robots"].size();
+        for (int i = 0; i < noRobots; i++) {
+            receiversHostname.push_back(commandFile["Robots"][i]["host"].asString());
+        }
+        
+        int commandsNo = commandFile["RobotCommands"].size();
+        for (int i = 0; i < commandsNo; i++) {
+            commands.insert(std::pair<int, string>(commandFile["RobotCommands"][i]["id"].asInt(),commandFile["RobotCommands"][i]["command"].asString()));
+        }
+    }
+}
+//--------------------------------------------------------------
+void RobotManager::fireCommand(int whichRobot, int command)
+{
+    ofxOscMessage e;
+    e.setAddress("/robot/"+commands[command]);
+    senders[receiversHostname[whichRobot]].sendMessage(e);
+    string cmds = "Robot "+ofToString(whichRobot) + " "+commands[command] + " " + ofToString(command);
+    commandsSent[whichRobot].push_back(cmds);
+}
+//--------------------------------------------------------------
+void RobotManager::draw(int x, int y)
+{
+    ofPushMatrix();
+    ofTranslate(x, y);
+    for (int i = 0; i < commandsSent.size(); i++) {
+        if (commandsSent[i].size() > 10) {
+            commandsSent[i].pop_front();
+        }
+    }
+    
+    for (int i = 0; i < commandsSent.size(); i++) {
+        for (int cm = 0; cm < commandsSent[i].size(); cm++) {
+            ofSetColor(255);
+            ofDrawBitmapString(commandsSent[i][cm], 15+(i*200), 15+(cm*15));
+        }
+    }
+    ofPopMatrix();
+}
+//--------------------------------------------------------------
+void RobotManager::close()
+{
+    for (int i = 0; i < senders.size(); i++) {
+        senders[receiversHostname[i]].disableBroadcast();
+    }
+}
--- H_A_R_P_S/src/local_addons/RobotManager.h
+++ H_A_R_P_S/src/local_addons/RobotManager.h
@@ -13,46 +13,53 @@
 #include <stdio.h>
 #include "ofMain.h"
 #include "ofxOsc.h"
+#include "ofxJSON.h"
 
 //-----------------------
-// * reduceHealth = reH;
-// * resetHealth = reset;
-// * malfunction = malf;
-// * start = str;
-// * instructions = inst;
-// * flashLights = fL;
-// * turnOffScreen = tIOS;
-// * turnOnScreen = tOOS;
-// * extendAerial = eA;
-// * retractAerial = rA;
+// * reduceHealth = RreH;
+// * resetHealth = Rreset;
+// * malfunction = Rmalf;
+// * start = Rstr;
+// * instructions = Rinst;
+// * flashLights = RfL;
+// * turnOffScreen = RtIOS;
+// * turnOnScreen = RtOOS;
+// * extendAerial = ReA;
+// * retractAerial = RrA;
 //-----------------------
-struct Commands {
-    string reduceHealth;
-    string resetHealth;
-    string malfunction;
-    string start;
-    string instructions;
-    string flashLights;
-    string turnOffScreen;
-    string turnOnScreen;
-    string extendAerial;
-    string retractAerial;
+enum ROBOT_COMMAND {
+    HPL_REDUCE_HEALTH = 0,
+    HPL_RESET_HEALTH,
+    HPL_MALFUNCTION,
+    HPL_START,
+    HPL_INSTRUCTIONS,
+    HPL_FLASH_LIGHTS,
+    HPL_TURN_OFF_SCREEN,
+    HPL_TURN_ON_SCREEN,
+    HPL_EXTEND_AERIAL,
+    HPL_RETRACT_AERIAL,
+    HPL_IN_DANGER,
+    HPL_IS_OK
 };
+
 
 class RobotManager {
 
-public:
-    void setup(string sendingTo, int port,string commandsFile);
-    void loadCommands(Commands commandStrings);
-    void update();
-    void fireCommand(int enumCommand);
-    void draw(int x, int y);
-    void close();
-    
-protected:
-    string _sendingTo;
-    int _port;
-    Commands commands;
+    public:
+        void setup();
+        void loadCommands();
+        void update();
+        void fireCommand(int whichRobot, int command);
+        void draw(int x, int y);
+        void close();
+        
+    protected:
+        vector <string> sendingTo;
+        int _port;
+        map<int,string> commands;
+        vector <string> receiversHostname;
+        vector <deque<string> > commandsSent;
+        map<string,ofxOscSender> senders;
 };
 
 #endif /* RobotManager_h */
